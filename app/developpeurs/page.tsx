8316d273f2a512bac302fc2ab3d40886b63fa50c
JordanCode <christjordan53@gmail.com>
2026-06-30 01:41:28 +0100

    initialisation du html, modification du globals

--- app/developpeurs/page.tsx
+++ app/developpeurs/page.tsx
@@ -2,8 +2,193 @@ import "./page.css";
 
 export default function developpeurs() {
   return (
-      <main>
-          <h1 className="bonjour text-8xl text-black">Hello world</h1>
-      </main>
+    <main className="bg-bg min-h-screen font-kode-mono text-primary p-8">
+        <h3>
+            Tous les développeurs
+        </h3>
+
+        <section>
+            <div>
+                <img src="" alt="" />
+                <h3>Sterge Dorsel MBADINGA MBADINGA</h3>
+                <span>Developpeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Abdoul BAKARY</h3>
+                <span>Developpeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Dodge Elfry NGUIA</h3>
+                <span>Developpeur Full-stack</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Marva Lorene NSA</h3>
+                <span>Developpeur Full-stack</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Christ Jordan OBIANG</h3>
+                <span>Developpeur Font-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Fatoumata NSANGOU</h3>
+                <span>Developpeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Jennisca NGUINA</h3>
+                <span>Dveloppeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Auceal Dorcas LAMAH ANOUMBA</h3>
+                <span>Developpeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Nancy MALAMENOU TSAGOULELA</h3>
+                <span>Developpeur Front-end</span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h3>Daliah OBAME</h3>
+                <span></span>
+                <ul>
+                    <li>Figma</li>
+                    <li>VS Code</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Node.js</li>
+                    <li>Vite</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <a href="">Voir le Portfolio</a>
+            </div>
+        </section>
+    </main>
   )
 }
