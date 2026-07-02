0a16d00e02b26048abddce4d43754373bbdb5b78
DodgeElfry <nguiadodge@gmail.com>
2026-07-02 04:29:13 +0100

    modification des tables et ajout des données initiales des développeurs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -14,6 +14,6 @@ function createPrismaClient() {
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
 }
